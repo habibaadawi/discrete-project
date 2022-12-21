5e9fd7c114bb2dfb83805bd753030327ee982d05
habibaadawi <119475203+habibaadawi@users.noreply.github.com>
2022-12-21 18:11:52 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,40 +6,41 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            // first prompt the user for the start and the end numbers 
-            Console.WriteLine("Enter the start number =  ");
-            int n1 = int.Parse(Console.ReadLine());
+            int num, count, prime, temp; // I assigned these variables at the beginning for clarity and ease when using any of them. 
 
-            Console.WriteLine("Enter the end number =  ");
-            int n2 = int.Parse(Console.ReadLine());
+            // prompt the user for the start and the end values 
+            Console.WriteLine("Enter start value = ");
+              int start = int.Parse(Console.ReadLine());
 
-            // then use the for loop to display every perfect number in the given range 
-            for (int i = n1; n1 <= n2; n1++)
+            Console.WriteLine("Enter end  value = ");
+              int end = int.Parse(Console.ReadLine());
+
+            // I used this condition to swap the start and the end values ,if needed just to avoid confusion. 
+            if (start > end)
             {
-                
-                int result = perfectnumber(n1); // is another method to find out wether n1 is a perfect number or not 
-
-                if (n1 == result) // finally use this condition to check if n1 equals to the sum returned from *perfectnumber* method
-                {
-                    Console.WriteLine("{0} is a perfect number", n1); 
-                }
-
+                temp = start;
+                start = end;
+                end = temp;
             }
+            Console.WriteLine(" Prime numbers between {0} and {1} is ", start, end);
 
-        }
-
-        static int perfectnumber(int n1)
-        {
-            int sum = 0;
-
-            for (int i = 1; i < n1; i++) 
+            // I used nested loop to find out and display the prime numbers in the given range.
+            for (num = start; num <= end; num++)
             {
-                if (n1 % i == 0)
+                prime = 0; // means that the the current num is a prime numb so far.
+                for (count = 2; count < num; count++)
                 {
-                    sum += i; // that is the sum of the divisors that are less than n1 
+                    if (num % count == 0)
+                    {
+                        prime = 1; // It means that this num is not a prime number .
+                        break;    // I used break here because there is no need to continue this loop. 
+                    }
+                }
+                if (prime == 0)
+                {
+                    Console.WriteLine(num);
                 }
             }
-            return sum;
         }
     }
 }
